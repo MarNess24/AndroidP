b86a0b5843a37872709d4c1c43651c10ce39d3b9
Vanessa <laylavanessa11@gmail.com>
2024-09-06 13:35:41 -0600

    Quité una d que puse de más

--- PruebaGitHub/app/src/main/java/com/example/pruebagithub/MainActivity.kt
+++ PruebaGitHub/app/src/main/java/com/example/pruebagithub/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             PruebaGitHubTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Androidd",
+                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
